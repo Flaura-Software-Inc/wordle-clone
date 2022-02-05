33cae43f2134fd5d69b4a9caf240baac8d697ada
Flinn Burgess <flinnburgess@gmail.com>
2022-02-05 16:22:51 +0000

    Add prettier files

--- App.tsx
+++ App.tsx
@@ -1,19 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 const Guess = () => {
-  return <View style={{width: '100%', flexDirection: 'row', justifyContent: 'center', marginTop: 10}}>
-    <View style={styles.guessBox}><Text style={styles.letter}>H</Text></View>
-    <View style={styles.guessBox}><Text style={styles.letter}>H</Text></View>
-    <View style={styles.guessBox}><Text style={styles.letter}>H</Text></View>
-    <View style={styles.guessBox}><Text style={styles.letter}>H</Text></View>
-    <View style={styles.guessBox}><Text style={styles.letter}>H</Text></View>
-  </View>
-}
+  return (
+    <View style={styles.guess}>
+      <View style={styles.guessBox}>
+        <Text style={styles.letter}>H</Text>
+      </View>
+      <View style={styles.guessBox}>
+        <Text style={styles.letter}>H</Text>
+      </View>
+      <View style={styles.guessBox}>
+        <Text style={styles.letter}>H</Text>
+      </View>
+      <View style={styles.guessBox}>
+        <Text style={styles.letter}>H</Text>
+      </View>
+      <View style={styles.guessBox}>
+        <Text style={styles.letter}>H</Text>
+      </View>
+    </View>
+  );
+};
 
 export default function App() {
   return (
-    <View style={[styles.container, {justifyContent: 'center'}]}>
+    <View style={[styles.container, { justifyContent: "center" }]}>
       <Guess />
       <Guess />
       <Guess />
@@ -27,21 +39,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  guess: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 10,
   },
   guessBox: {
     width: 50,
     height: 50,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     marginLeft: 10,
     borderWidth: 2,
-    borderColor: '#a9a9a9',
-    alignItems: 'center',
-    justifyContent: 'center',
+    borderColor: "#a9a9a9",
+    alignItems: "center",
+    justifyContent: "center",
   },
   letter: {
-    fontSize: 25
-  }
+    fontSize: 25,
+  },
 });
