de7b77a54b65deaa98d8910034d0887eb236f700
Flinn Burgess <flinnburgess@gmail.com>
2022-02-05 15:59:12 +0000

    add basic box layout

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Guess = () => {
+  return <View style={{width: '100%', flexDirection: 'row', justifyContent: 'center', marginTop: 10}}>
+    <View style={styles.guessBox}><Text style={styles.letter}>H</Text></View>
+    <View style={styles.guessBox}><Text style={styles.letter}>H</Text></View>
+    <View style={styles.guessBox}><Text style={styles.letter}>H</Text></View>
+    <View style={styles.guessBox}><Text style={styles.letter}>H</Text></View>
+    <View style={styles.guessBox}><Text style={styles.letter}>H</Text></View>
+  </View>
+}
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, {justifyContent: 'center'}]}>
+      <Guess />
+      <Guess />
+      <Guess />
+      <Guess />
+      <Guess />
+      <Guess />
     </View>
   );
 }
@@ -17,4 +31,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  guessBox: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'white',
+    marginLeft: 10,
+    borderWidth: 2,
+    borderColor: '#a9a9a9',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  letter: {
+    fontSize: 25
+  }
 });
