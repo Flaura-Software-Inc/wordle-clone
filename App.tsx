bc4d7fc8ccaeb2e0879922a5d0e892085f7f1f11
Flinn Burgess <flinnburgess@gmail.com>
2022-02-08 08:35:30 +0000

    Correctly highlight letters on guess

--- App.tsx
+++ App.tsx
@@ -5,51 +5,91 @@ import { StyleSheet, Text, View } from "react-native";
 type GuessProps = {
   value: string;
   submitted: boolean;
-  answerLetterMap: Map<string, number[]>;
+  answer: string;
 };
 
-const Guess = ({ value, submitted, answerLetterMap }: GuessProps) => {
-  const getBackgroundColour = (letterIdx: number) => {
-    if (!submitted) {
-      return styles.whiteBackground;
-    }
-
-    const letter = value[letterIdx].toUpperCase();
-
-    if (answerLetterMap.has(letter)) {
-      if (answerLetterMap.get(letter)!.includes(letterIdx)) {
-        return styles.greenBackground;
+const createLetterMap = (answer: string) => {
+  const letterMap: Map<string, number[]> = new Map();
+  answer
+    .toUpperCase()
+    .split("")
+    .forEach((char, i) => {
+      if (letterMap.has(char)) {
+        letterMap.get(char)!.push(i);
+      } else {
+        letterMap.set(char, [i]);
       }
-      return styles.yellowBackground;
-    } else {
-      return styles.greyBackground;
-    }
-  };
+    });
+  return letterMap;
+};
+
+const Guess = ({ value, submitted, answer }: GuessProps) => {
+  const answerLetterMap = createLetterMap(answer);
+  const tileColours = [
+    styles.whiteBackground,
+    styles.whiteBackground,
+    styles.whiteBackground,
+    styles.whiteBackground,
+    styles.whiteBackground,
+  ];
+
+  if (submitted) {
+    const guessLetterMap: Map<string, number[]> = createLetterMap(value);
+
+    Array.from(guessLetterMap.keys()).forEach((char) => {
+      let charIndexes = guessLetterMap.get(char);
+
+      if (!answerLetterMap.has(char)) {
+        charIndexes!.forEach(
+          (charIdx) => (tileColours[charIdx] = styles.greyBackground)
+        );
+      } else {
+        charIndexes!.forEach((charIdx) => {
+          if (answerLetterMap.get(char)!.includes(charIdx)) {
+            tileColours[charIdx] = styles.greenBackground;
+            charIndexes = charIndexes!.filter((idx) => idx !== charIdx);
+            answerLetterMap.set(
+              char,
+              answerLetterMap.get(char)!.filter((idx) => idx !== charIdx)
+            );
+          }
+        });
+        charIndexes!.forEach((charIdx, i) => {
+          if (i < answerLetterMap.get(char)!.length) {
+            tileColours[charIdx] = styles.yellowBackground;
+          } else {
+            tileColours[charIdx] = styles.greyBackground;
+          }
+        });
+      }
+    });
+  }
+
   value = value.toUpperCase();
-  // set outline to black for current letter
+  // set outline to black for filled letter
   return (
     <View style={styles.guess}>
-      <View style={[styles.guessBox, getBackgroundColour(0)]}>
+      <View style={[styles.guessBox, tileColours[0]]}>
         <Text style={[styles.letter, submitted && styles.whiteText]}>
           {value[0] ?? ""}
         </Text>
       </View>
-      <View style={[styles.guessBox, getBackgroundColour(1)]}>
+      <View style={[styles.guessBox, tileColours[1]]}>
         <Text style={[styles.letter, submitted && styles.whiteText]}>
           {value[1] ?? ""}
         </Text>
       </View>
-      <View style={[styles.guessBox, getBackgroundColour(2)]}>
+      <View style={[styles.guessBox, tileColours[2]]}>
         <Text style={[styles.letter, submitted && styles.whiteText]}>
           {value[2] ?? ""}
         </Text>
       </View>
-      <View style={[styles.guessBox, getBackgroundColour(3)]}>
+      <View style={[styles.guessBox, tileColours[3]]}>
         <Text style={[styles.letter, submitted && styles.whiteText]}>
           {value[3] ?? ""}
         </Text>
       </View>
-      <View style={[styles.guessBox, getBackgroundColour(4)]}>
+      <View style={[styles.guessBox, tileColours[4]]}>
         <Text style={[styles.letter, submitted && styles.whiteText]}>
           {value[4] ?? ""}
         </Text>
@@ -120,26 +160,11 @@ const Keyboard = ({ onKeyPress, onDelete, onSubmit }: KeyboardProps) => {
   );
 };
 
-const createAnswerLetterMap = (answer: string) => {
-  const letterMap: Map<string, number[]> = new Map();
-  answer
-    .toUpperCase()
-    .split("")
-    .forEach((char, i) => {
-      if (letterMap.has(char)) {
-        letterMap.get(char)!.push(i);
-      } else {
-        letterMap.set(char, [i]);
-      }
-    });
-  return letterMap;
-};
-
 export default function App() {
   const [guesses, setGuesses] = useState<string[]>([]);
   const [currentGuess, setCurrentGuess] = useState("");
   const answer = "HELLO";
-  const answerLetterMap = createAnswerLetterMap(answer);
+  const answerLetterMap = createLetterMap(answer);
   const onSubmit = () => {
     if (currentGuess.length < 5) {
       return;
@@ -164,32 +189,32 @@ export default function App() {
       <Guess
         value={guesses[0] ?? (guesses.length === 0 ? currentGuess : "")}
         submitted={guesses.length >= 1}
-        answerLetterMap={answerLetterMap}
+        answer={answer}
       />
       <Guess
         value={guesses[1] ?? (guesses.length === 1 ? currentGuess : "")}
         submitted={guesses.length >= 2}
-        answerLetterMap={answerLetterMap}
+        answer={answer}
       />
       <Guess
         value={guesses[2] ?? (guesses.length === 2 ? currentGuess : "")}
         submitted={guesses.length >= 3}
-        answerLetterMap={answerLetterMap}
+        answer={answer}
       />
       <Guess
         value={guesses[3] ?? (guesses.length === 3 ? currentGuess : "")}
         submitted={guesses.length >= 4}
-        answerLetterMap={answerLetterMap}
+        answer={answer}
       />
       <Guess
         value={guesses[4] ?? (guesses.length === 4 ? currentGuess : "")}
         submitted={guesses.length >= 5}
-        answerLetterMap={answerLetterMap}
+        answer={answer}
       />
       <Guess
         value={guesses[5] ?? (guesses.length === 5 ? currentGuess : "")}
         submitted={guesses.length >= 6}
-        answerLetterMap={answerLetterMap}
+        answer={answer}
       />
       <Keyboard
         onKeyPress={onKeyPress}
